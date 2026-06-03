25b2c43d41277b3a7d167fb29760ebf68266d9f7
艾嘉琪 <17094467+ai-jiaqi@user.noreply.gitee.com>
2026-06-03 14:32:11 +0800

    重构：精简实体子类（Book/Magazine/DVD/EBook），类型特有属性迁移至extra_attrs JSONB字段

--- src/main/java/com/library/model/entity/EBook.java
+++ src/main/java/com/library/model/entity/EBook.java
@@ -7,34 +7,14 @@ import jakarta.persistence.Entity;
 @DiscriminatorValue("EBOOK")
 public class EBook extends LibraryResource {
 
-    private String format;
-    private Long fileSizeMB;
-    private String downloadUrl;
-
     protected EBook() {}
 
-    public EBook(String id, String title, String format,
-                 long fileSizeMB, String downloadUrl) {
-        super(id, title);
-        this.format = format;
-        this.fileSizeMB = fileSizeMB;
-        this.downloadUrl = downloadUrl;
+    public EBook(String id, String title) {
+        super(id, title, "EBOOK");
     }
 
     @Override
     public int getMaxBorrowDays() {
         return 21;
     }
-
-    @Override
-    public String getResourceType() {
-        return "EBOOK";
-    }
-
-    public String getFormat() { return format; }
-    public void setFormat(String format) { this.format = format; }
-    public Long getFileSizeMB() { return fileSizeMB; }
-    public void setFileSizeMB(Long fileSizeMB) { this.fileSizeMB = fileSizeMB; }
-    public String getDownloadUrl() { return downloadUrl; }
-    public void setDownloadUrl(String downloadUrl) { this.downloadUrl = downloadUrl; }
 }
--- src/main/java/com/library/model/entity/Magazine.java
+++ src/main/java/com/library/model/entity/Magazine.java
@@ -2,40 +2,19 @@ package com.library.model.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import java.time.LocalDate;
 
 @Entity
 @DiscriminatorValue("MAGAZINE")
 public class Magazine extends LibraryResource {
 
-    private String issueNumber;
-    private LocalDate publishDate;
-    private String category;
-
     protected Magazine() {}
 
-    public Magazine(String id, String title, String issueNumber,
-                    LocalDate publishDate, String category) {
-        super(id, title);
-        this.issueNumber = issueNumber;
-        this.publishDate = publishDate;
-        this.category = category;
+    public Magazine(String id, String title) {
+        super(id, title, "MAGAZINE");
     }
 
     @Override
     public int getMaxBorrowDays() {
         return 14;
     }
-
-    @Override
-    public String getResourceType() {
-        return "MAGAZINE";
-    }
-
-    public String getIssueNumber() { return issueNumber; }
-    public void setIssueNumber(String issueNumber) { this.issueNumber = issueNumber; }
-    public LocalDate getPublishDate() { return publishDate; }
-    public void setPublishDate(LocalDate publishDate) { this.publishDate = publishDate; }
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
 }
