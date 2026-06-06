6e7eadb8dc66ec54f41ab425b53479a1e6e465c6
luolangaga <luolan233@outlook.com>
2026-06-06 11:05:44 +0800

    重构：ResourceFactory改为静态create(DTO)方法，适配JSONB数据模型

--- src/main/java/com/library/model/factory/ResourceFactory.java
+++ src/main/java/com/library/model/factory/ResourceFactory.java
@@ -1,73 +1,51 @@
 package com.library.model.factory;
 
+import com.library.model.dto.ResourceDTO;
 import com.library.model.entity.*;
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class ResourceFactory {
 
-    private final Map<String, ResourceCreator> creators;
+    public static LibraryResource create(ResourceDTO dto) {
+        String type = dto.getType();
+        Map<String, Object> attrs = dto.getExtraAttrs() != null
+                ? dto.getExtraAttrs() : new HashMap<>();
 
-    @FunctionalInterface
-    public interface ResourceCreator {
-        LibraryResource create(Map<String, String> params);
-    }
+        LibraryResource resource = switch (type) {
+            case "BOOK" -> new Book(dto.getId(), dto.getTitle());
+            case "MAGAZINE" -> new Magazine(dto.getId(), dto.getTitle());
+            case "DVD" -> new DVD(dto.getId(), dto.getTitle());
+            case "EBOOK" -> new EBook(dto.getId(), dto.getTitle());
+            default -> throw new IllegalArgumentException("未知资源类型: " + type);
+        };
 
-    public ResourceFactory() {
-        this.creators = new HashMap<>();
-        register("BOOK", this::createBook);
-        register("MAGAZINE", this::createMagazine);
-        register("DVD", this::createDVD);
-        register("EBOOK", this::createEBook);
-    }
-
-    public void register(String type, ResourceCreator creator) {
-        creators.put(type, creator);
-    }
-
-    public LibraryResource createResource(String type, Map<String, String> params) {
-        ResourceCreator creator = creators.get(type);
-        if (creator == null) {
-            throw new IllegalArgumentException("未知资源类型: " + type);
+        Map<String, Object> extra = new HashMap<>();
+        if ("BOOK".equals(type)) {
+            if (attrs.containsKey("author")) extra.put("author", attrs.get("author"));
+            if (attrs.containsKey("isbn")) extra.put("isbn", attrs.get("isbn"));
+            if (attrs.containsKey("publisher")) extra.put("publisher", attrs.get("publisher"));
+            if (attrs.containsKey("pages")) extra.put("pages", attrs.get("pages"));
+        } else if ("MAGAZINE".equals(type)) {
+            if (attrs.containsKey("issueNumber")) extra.put("issueNumber", attrs.get("issueNumber"));
+            if (attrs.containsKey("publishDate")) extra.put("publishDate", attrs.get("publishDate"));
+            if (attrs.containsKey("category")) extra.put("category", attrs.get("category"));
+        } else if ("DVD".equals(type)) {
+            if (attrs.containsKey("director")) extra.put("director", attrs.get("director"));
+            if (attrs.containsKey("durationMinutes")) extra.put("durationMinutes", attrs.get("durationMinutes"));
+            if (attrs.containsKey("genre")) extra.put("genre", attrs.get("genre"));
+        } else if ("EBOOK".equals(type)) {
+            if (attrs.containsKey("format")) extra.put("format", attrs.get("format"));
+            if (attrs.containsKey("fileSizeMB")) extra.put("fileSizeMB", attrs.get("fileSizeMB"));
+            if (attrs.containsKey("downloadUrl")) extra.put("downloadUrl", attrs.get("downloadUrl"));
         }
-        return creator.create(params);
+
+        resource.setExtraAttrs(extra);
+        return resource;
     }
 
-    public boolean isTypeSupported(String type) {
-        return creators.containsKey(type);
-    }
-
-    public Set<String> getSupportedTypes() {
-        return creators.keySet();
-    }
-
-    private LibraryResource createBook(Map<String, String> p) {
-        return new Book(p.get("id"), p.get("title"),
-                p.get("author"), p.get("isbn"),
-                p.get("publisher"),
-                Integer.parseInt(p.getOrDefault("pages", "0")));
-    }
-
-    private LibraryResource createMagazine(Map<String, String> p) {
-        return new Magazine(p.get("id"), p.get("title"),
-                p.get("issueNumber"),
-                LocalDate.parse(p.get("publishDate")),
-                p.get("category"));
-    }
-
-    private LibraryResource createDVD(Map<String, String> p) {
-        return new DVD(p.get("id"), p.get("title"),
-                p.get("director"),
-                Integer.parseInt(p.getOrDefault("duration", "0")),
-                p.get("genre"));
-    }
-
-    private LibraryResource createEBook(Map<String, String> p) {
-        return new EBook(p.get("id"), p.get("title"),
-                p.get("format"),
-                Long.parseLong(p.getOrDefault("fileSizeMB", "0")),
-                p.get("downloadUrl"));
+    public static boolean isTypeSupported(String type) {
+        return "BOOK".equals(type) || "MAGAZINE".equals(type)
+                || "DVD".equals(type) || "EBOOK".equals(type);
     }
 }
