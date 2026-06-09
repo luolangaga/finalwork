6fdbaaa30e8db8574e509aa8f6a021c3cdb7c26f
宋婧怡517 <17037052+songjingyi517@user.noreply.gitee.com>
2026-06-09 13:14:52 +0800

    重构：ResourceService添加RabbitMQ事件发布，资源增删操作发送异步消息

--- src/main/java/com/library/service/ResourceService.java
+++ src/main/java/com/library/service/ResourceService.java
@@ -1,16 +1,16 @@
 package com.library.service;
 
+import com.library.config.RabbitMQConfig;
 import com.library.model.dto.ResourceDTO;
 import com.library.model.entity.*;
 import com.library.model.factory.ResourceFactory;
 import com.library.repository.ResourceRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,7 +19,8 @@ public class ResourceService {
     @Autowired
     private ResourceRepository resourceRepo;
 
-    private final ResourceFactory factory = new ResourceFactory();
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     public List<ResourceDTO> findAll() {
         return resourceRepo.findAll().stream()
@@ -48,61 +49,41 @@ public class ResourceService {
 
     @Transactional
     public ResourceDTO addResource(ResourceDTO dto) {
-        Map<String, String> params = new HashMap<>();
-        params.put("id", dto.getId());
-        params.put("title", dto.getTitle());
-        if (dto.getAuthor() != null) params.put("author", dto.getAuthor());
-        if (dto.getIsbn() != null) params.put("isbn", dto.getIsbn());
-        if (dto.getPublisher() != null) params.put("publisher", dto.getPublisher());
-        if (dto.getPages() != null) params.put("pages", String.valueOf(dto.getPages()));
-        if (dto.getIssueNumber() != null) params.put("issueNumber", dto.getIssueNumber());
-        if (dto.getPublishDate() != null) params.put("publishDate", dto.getPublishDate());
-        if (dto.getCategory() != null) params.put("category", dto.getCategory());
-        if (dto.getDirector() != null) params.put("director", dto.getDirector());
-        if (dto.getDurationMinutes() != null) params.put("duration", String.valueOf(dto.getDurationMinutes()));
-        if (dto.getGenre() != null) params.put("genre", dto.getGenre());
-        if (dto.getFormat() != null) params.put("format", dto.getFormat());
-        if (dto.getFileSizeMB() != null) params.put("fileSizeMB", String.valueOf(dto.getFileSizeMB()));
-        if (dto.getDownloadUrl() != null) params.put("downloadUrl", dto.getDownloadUrl());
-
-        LibraryResource resource = factory.createResource(dto.getType(), params);
+        LibraryResource resource = ResourceFactory.create(dto);
         resourceRepo.save(resource);
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.LIBRARY_EXCHANGE,
+                "resource.created",
+                java.util.Map.of("resourceId", resource.getId(),
+                        "type", resource.getType())
+        );
+
         return convertToDTO(resource);
     }
 
     @Transactional
     public void deleteResource(String id) {
         resourceRepo.deleteById(id);
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.LIBRARY_EXCHANGE,
+                "resource.deleted",
+                java.util.Map.of("resourceId", id)
+        );
     }
 
     private ResourceDTO convertToDTO(LibraryResource r) {
         ResourceDTO dto = new ResourceDTO();
         dto.setId(r.getId());
         dto.setTitle(r.getTitle());
-        dto.setType(r.getResourceType());
+        dto.setType(r.getType());
         dto.setStatus(r.getStatus().name());
         dto.setBorrowerId(r.getBorrowerId());
         if (r.getBorrowDate() != null) dto.setBorrowDate(r.getBorrowDate().toString());
         if (r.getDueDate() != null) dto.setDueDate(r.getDueDate().toString());
-
-        if (r instanceof Book book) {
-            dto.setAuthor(book.getAuthor());
-            dto.setIsbn(book.getIsbn());
-            dto.setPublisher(book.getPublisher());
-            dto.setPages(book.getPages());
-        } else if (r instanceof Magazine mag) {
-            dto.setIssueNumber(mag.getIssueNumber());
-            if (mag.getPublishDate() != null) dto.setPublishDate(mag.getPublishDate().toString());
-            dto.setCategory(mag.getCategory());
-        } else if (r instanceof DVD dvd) {
-            dto.setDirector(dvd.getDirector());
-            dto.setDurationMinutes(dvd.getDurationMinutes());
-            dto.setGenre(dvd.getGenre());
-        } else if (r instanceof EBook ebook) {
-            dto.setFormat(ebook.getFormat());
-            dto.setFileSizeMB(ebook.getFileSizeMB());
-            dto.setDownloadUrl(ebook.getDownloadUrl());
-        }
+        dto.setBorrowDays(r.getMaxBorrowDays());
+        dto.setExtraAttrs(r.getExtraAttrs());
         return dto;
     }
 }
